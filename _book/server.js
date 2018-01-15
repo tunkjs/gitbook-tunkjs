0845d43d1eff012403309d0e81f8b9ce73b16ba2
steven03.chen <steven03.chen@vipshop.com>
2018-01-15 23:45:01 +0800

    no message

--- _book/server.js
+++ _book/server.js
@@ -1,8 +1,10 @@
-const serve = require('koa-static');
+const serve = require('koa-static-cache');
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(serve(__dirname + '/_book'));
+app.use(serve(__dirname + '/_book', {
+    maxAge: 60 * 60
+}));
 
 app.listen(3000);
 
